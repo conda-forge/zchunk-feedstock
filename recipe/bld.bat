f3d142a6677772be67a2f5d373cfe143e2ecbcd9
David Brochart <david.brochart@gmail.com>
2020-12-02 13:51:52 +0100

    Patch for linking with argp for OSX

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
-meson builddir --prefix="%PREFIX%" --libdir="%LIBRARY_PREFIX%"
+meson builddir -D c_std=c99 --prefix="%PREFIX%" --libdir="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
 
-meson configure -D enable_docs=false builddir
+meson configure -D enable_docs=false -D c_std=c99 builddir
 if errorlevel 1 exit 1
 
 ninja -v -C builddir
