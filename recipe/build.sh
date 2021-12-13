c0444ad118f09e294f9c174392479e0461452f58
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-12-13 17:48:18 +0100

    use pkg_config in build prefix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 set -ex
 
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
 meson setup builddir \
     ${MESON_ARGS} \
     --buildtype=release \
